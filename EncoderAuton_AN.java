b13daf60ae1eda95cd2487614858dfaffa89cec2
ananya4198 <115587326+ananya4198@users.noreply.github.com>
2024-01-09 18:55:45 -0500

    Add files via upload

--- EncoderAuton_AN.java
+++ EncoderAuton_AN.java
@@ -7,37 +7,52 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 @Autonomous
-public class EncoderAuton_AN extends LinearOpMode {
-    public DcMotor BR, BL, FR, FL;
-    public Servo arm;
+public class EncoderAuton_AN extends LinearOpMode
+{
+    public DcMotor BR, BL, FR, FL, Arm0, Arm1;
 
     //clicks per inch
     //CPI is calculated by ((Clicks of Encoders per revolution)/(diameter*pi   make sure it is in inches))
     //Clicks of Encoders per revolution may change based off of different model types
     //The smaller the diameter of the more clicks needed, the larger the diameter less clicks are needed
-    double cpi = 5;
+    //goBilda motor encoder resolution or CPR : 145.6 OR 233
+    //diamater of wheels : 96 mm / 3.77953 inches
+    //145.6 / (3.77953 * pi) = 12.262
+    double cpi = 7.7; //18.78
 
     //clicks per inch for linear slides since different diameter than wheels and different motor
-    double lcpi = 68;
+    //double lcpi = 68;
 
     //clicks per degree
-    double cpd = 4;
+    double cpd = 4; //3.19444444444
 
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode() throws InterruptedException
+    {
         //Initialization of motors and servos
 
         BR = hardwareMap.get(DcMotor.class, "BR");
         BL = hardwareMap.get(DcMotor.class, "BL");
         FR = hardwareMap.get(DcMotor.class, "FR");
         FL = hardwareMap.get(DcMotor.class, "FL");
-        arm = hardwareMap.get(Servo.class, "Arm");
+        Arm0 = hardwareMap.get(DcMotor.class, "Arm0");
+        Arm1 = hardwareMap.get(DcMotor.class, "Arm1");
 
-        BR.setDirection(DcMotorSimple.Direction.FORWARD);
-        FR.setDirection(DcMotorSimple.Direction.FORWARD);
-        BL.setDirection(DcMotorSimple.Direction.REVERSE);
-        FL.setDirection(DcMotorSimple.Direction.REVERSE);
-        arm.setDirection(Servo.Direction.REVERSE);
+
+        FR.setDirection(DcMotorSimple.Direction.REVERSE);
+        FL.setDirection(DcMotorSimple.Direction.FORWARD);
+        BR.setDirection(DcMotorSimple.Direction.REVERSE);
+        BL.setDirection(DcMotorSimple.Direction.FORWARD);
+        Arm0.setDirection(DcMotorSimple.Direction.REVERSE);
+        Arm1.setDirection(DcMotorSimple.Direction.FORWARD);
+
+
+        BR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        FR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        FL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
 
         BR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         BR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -51,22 +66,32 @@ public class EncoderAuton_AN extends LinearOpMode {
         FL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         FL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
+        /*
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+         */
+
         BR.setPower(0);
         FR.setPower(0);
         BL.setPower(0);
         FL.setPower(0);
-        arm.setPosition(0);
+        Arm0.setPower(0);
+        Arm1.setPower(0);
 
-      waitForStart();
-      sleep(200);
-      forward (2, 0.03);
-      sleep(200);
-/*
+        waitForStart();
+
+        //Closing claw
+        //claw.setPosition(0.45);
+        forward(2, 0.2);
+        //Opening claw
+        //claw.setPosition(0);
+
         //Forward 12 inches
-        forward(12, 0.4);
+        /*
+        forward(44, 0.4);
 
         //Backward 12 inches
-        backward(12,0.4);
+        backward(8,0.4);
 
         //Right 90 degrees
         right(90,0.4);
@@ -75,29 +100,27 @@ public class EncoderAuton_AN extends LinearOpMode {
         left(90,0.4);
 
         //Linear slides up 12 inches
-        linearup(12,0.6);
+        //linearup(12,0.6);
 
         //Linear slides down 12 inches
-        lineardown(12,0.6);
-
- */
-
+        //lineardown(12,0.6);
+*/
         //Stops the robot for the rest of auton
         BR.setPower(0);
         FR.setPower(0);
         BL.setPower(0);
         FL.setPower(0);
-        arm.setPosition(0);
-
+        Arm0.setPower(0);
+        Arm1.setPower(0);
     }
 
-
-    private void forward(double inch, double power) {
+    private void forward(double inch,  double power)
+    {
         //Sets new position for motors
-        int a = (int) (FL.getCurrentPosition() + (inch * cpi));
-        int b = (int) (FR.getCurrentPosition() + (inch * cpi));
-        int c = (int) (BL.getCurrentPosition() + (inch * cpi));
-        int d = (int) (BR.getCurrentPosition() + (inch * cpi));
+        int a = (int) (FL.getCurrentPosition() + (inch*cpi));
+        int b = (int) (FR.getCurrentPosition() + (inch*cpi));
+        int c = (int) (BL.getCurrentPosition() + (inch*cpi));
+        int d = (int) (BR.getCurrentPosition() + (inch*cpi));
 
         //Creates a target position
         FL.setTargetPosition(a);
@@ -118,7 +141,8 @@ public class EncoderAuton_AN extends LinearOpMode {
         BR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         //Loop to run encoders method
-        while (FL.isBusy() && FR.isBusy() && BL.isBusy() && BR.isBusy()) {
+        while (FL.isBusy() && FR.isBusy() && BL.isBusy() && BR.isBusy())
+        {
             //Displays on the phone what the target position is vs the actual position
             telemetry.addLine("Move Forward");
             telemetry.addData("Target", "%7d :%7d : %7d : %7d", a, b, c, d);
@@ -140,12 +164,13 @@ public class EncoderAuton_AN extends LinearOpMode {
         BR.setPower(0);
     }
 
-    private void backward(double inch, double power) {
+    private void backward(double inch, double power)
+    {
         //Sets new position for motors
-        int a = (int) (FL.getCurrentPosition() - (inch * cpi));
-        int b = (int) (FR.getCurrentPosition() - (inch * cpi));
-        int c = (int) (BL.getCurrentPosition() - (inch * cpi));
-        int d = (int) (BR.getCurrentPosition() - (inch * cpi));
+        int a = (int) (FL.getCurrentPosition() - (inch*cpi));
+        int b = (int) (FR.getCurrentPosition() - (inch*cpi));
+        int c = (int) (BL.getCurrentPosition() - (inch*cpi));
+        int d = (int) (BR.getCurrentPosition() - (inch*cpi));
 
         FL.setTargetPosition(-a);
         FR.setTargetPosition(-b);
@@ -165,14 +190,15 @@ public class EncoderAuton_AN extends LinearOpMode {
         BR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         //Loop to run encoders method
-        while (FL.isBusy() && FR.isBusy() && BL.isBusy() && BR.isBusy()) {
+        while (FL.isBusy() && FR.isBusy() && BL.isBusy() && BR.isBusy())
+        {
             telemetry.addLine("Move Forward");
             telemetry.addData("Target", "%7d :%7d : %7d : %7d", a, b, c, d);
             telemetry.addData("Actual", "%7d :%7d : %7d : %7d", FL.getCurrentPosition(), FR.getCurrentPosition(), BL.getCurrentPosition(), BR.getCurrentPosition());
             telemetry.update();
         }
 
-
+        //Eliminates momentum
         FL.setPower(0.1);
         FR.setPower(0.1);
         BL.setPower(0.1);
@@ -184,8 +210,6 @@ public class EncoderAuton_AN extends LinearOpMode {
         FR.setPower(0);
         BL.setPower(0);
         BR.setPower(0);
-
-
     }
 
     private void right(double degree, double power) {
@@ -263,49 +287,62 @@ public class EncoderAuton_AN extends LinearOpMode {
         BL.setPower(0);
         BR.setPower(0);
     }
-}
+
 
     /*
-    private void linearup(double inch, double power)
+    private void armup(double degree, double power)
     {
-        int a = (int) (arm.getCurrentPosition() + (inch*lcpi));
-        arm.setTargetPosition(a);
-        arm.setPower(power);
-        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        while (arm.isBusy())
-        {
-            telemetry.addLine("Linear up");
-            telemetry.addData("Target", "%7d", a);
-            telemetry.addData("Actual", "%7d", arm.getCurrentPosition());
+        int a = (int) (Arm0.getCurrentPosition() + (inch*lcpi));
+        int b = (int) (Arm1.getCurrentPosition() + (inch*lcpi));
+        Arm0.setTargetPosition(a);
+        Arm1.setTargetPosition(b);
+        Arm0.setPower(power);
+        Arm1.setPower(power);
+        Arm0.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        Arm1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        while (Arm0.isBusy() && Arm1.isBusy()) {
+            telemetry.addLine("Arm up");
+            telemetry.addData("Target", "%7d :%7d, a, b);
+            telemetry.addData("Actual", "%7d": %7d, Arm0.getCurrentPosition(), Arm1.getCurrentPosition());
             telemetry.update();
         }
+    }
 
+        //PROBABLY DONT NEED THIS
+        /*
         //Changes power based on height to stop gravity from pulling down linear slides
         if(inch < 8)
         {
-            arm.setPower(0.1);
+            linear.setPower(0.1);
         }else if (inch >= 8 && inch < 18)
         {
-            arm.setPower(0.2);
+            linear.setPower(0.2);
         }else if(inch >= 18)
         {
-            arm.setPower(0.3);
+            linear.setPower(0.3);
         }
+        */
+
+    /*
     }
-    private void lineardown(double inch, double power)
+    private void armdown(double inch, double power)
     {
-        int a = (int) (arm.getCurrentPosition() - (inch*lcpi));
-        arm.setTargetPosition(a);
-        arm.setPower(power);
-        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        while (arm.isBusy())
-        {
-            telemetry.addLine("Linear up");
-            telemetry.addData("Target", "%7d", a);
-            telemetry.addData("Actual", "%7d", arm.getCurrentPosition());
+       int a = (int) (Arm0.getCurrentPosition() - (inch*lcpi));
+        int b = (int) (Arm1.getCurrentPosition() - (inch*lcpi));
+        Arm0.setTargetPosition(a);
+        Arm1.setTargetPosition(b);
+        Arm0.setPower(power);
+        Arm1.setPower(power);
+        Arm0.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        Arm1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        while (Arm0.isBusy() && Arm1.isBusy()) {
+            telemetry.addLine("Arm down");
+            telemetry.addData("Target", "%7d :%7d, a, b);
+            telemetry.addData("Actual", "%7d": %7d, Arm0.getCurrentPosition(), Arm1.getCurrentPosition());
             telemetry.update();
-        }
-        arm.setPower(0);
+         }
+        Arm0.setPower(0);
+        Arm1.setPower(0);
     }
-}
      */
+}
---
+++ Tele_AN.java
@@ -0,0 +1,155 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+public class Tele_AN extends OpMode
+{
+    private double powerLX, powerLY, powerRX, powerRY;
+    private DcMotor FL, BL, FR, BR, Arm0, Arm1;
+    private Servo Launcher;
+
+    @Override
+    public void init()
+    {
+        BR = hardwareMap.get(DcMotor.class, "BR");
+        BL = hardwareMap.get(DcMotor.class, "BL");
+        FR = hardwareMap.get(DcMotor.class, "FR");
+        FL = hardwareMap.get(DcMotor.class, "FL");
+        Arm0 = hardwareMap.get(DcMotor.class, "Arm0");
+        Arm1 = hardwareMap.get(DcMotor.class, "Arm1");
+        Launcher = hardwareMap.get(Servo.class, "Launcher");
+
+
+        FR.setDirection(DcMotorSimple.Direction.REVERSE);
+        FL.setDirection(DcMotorSimple.Direction.FORWARD);
+        BR.setDirection(DcMotorSimple.Direction.REVERSE);
+        BL.setDirection(DcMotorSimple.Direction.FORWARD);
+        Arm0.setDirection(DcMotorSimple.Direction.REVERSE);
+        Arm1.setDirection(DcMotorSimple.Direction.FORWARD);
+
+
+        BR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        FR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        FL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+        FR.setPower(0);
+        FL.setPower(0);
+        BR.setPower(0);
+        BL.setPower(0);
+        Arm0.setPower(0);
+        Arm1.setPower(0);
+        Launcher.setPosition(0);
+
+    }
+
+    @Override
+    public void loop()
+    {
+        powerLX = gamepad1.left_stick_x/2;
+        powerLY = gamepad1.left_stick_y/2;
+        powerRX = gamepad1.right_stick_x/2;
+        powerRY = gamepad1.right_stick_y/2;
+
+        if(powerLY > 0.05 || powerLY < -0.05)
+        {
+            FL.setPower(powerLY);
+            BL.setPower(powerLY);
+        } else
+        {
+            FL.setPower(0);
+            BL.setPower(0);
+        }
+
+        if(powerRY > 0.05 || powerRY < -0.05)
+        {
+            FR.setPower(powerRY);
+            BR.setPower(powerRY);
+        } else
+        {
+            FR.setPower(0);
+            BR.setPower(0);
+        }
+
+        if(gamepad1.right_bumper){
+            FL.setPower(-0.85);
+            BL.setPower(0.85);
+            FR.setPower(0.85);
+            BR.setPower(-0.85);
+        }
+        if(gamepad1.left_bumper){
+            FL.setPower(0.85);
+            BL.setPower(-0.85);
+            FR.setPower(-0.85);
+            BR.setPower(0.85);
+        }
+
+        //intake/outake
+
+        //going up
+        if (gamepad2.dpad_up)
+        {
+            Arm0.setPower(0.5);
+            Arm1.setPower(0.5);
+        }
+        //stopping when going up
+        if(gamepad2.dpad_right) {
+            Arm0.setPower(-0.15);
+            Arm1.setPower(-0.15);
+        }
+        //going down
+        if(gamepad2.dpad_down){
+            Arm0.setPower(-0.2);
+            Arm1.setPower(-0.2);
+        }
+        //stop when going down
+        if(gamepad2.dpad_left){
+            Arm0.setPower(0.07);
+            Arm1.setPower(0.07);
+        }
+        //stop TOTALLY / reset
+        if(gamepad2.x){
+            Arm0.setPower(0);
+            Arm1.setPower(0);
+        }
+        //to make it sit in the air
+        if(gamepad2.right_bumper){
+            Arm0.setPower(0.35);
+            Arm1.setPower(0.35);
+        }
+
+
+        //to shoot launcher
+        if(gamepad2.a){
+            Launcher.setPosition(0.4);
+        }
+
+        //reset launcher
+        if(gamepad2.y){
+            Launcher.setPosition(0);
+        }
+
+        // telemetry
+        telemetry.addData("powerRX: ", gamepad1.right_stick_x);
+        telemetry.addData("powerRY: ", gamepad1.right_stick_y);
+        telemetry.addData("powerLX: ", gamepad1.left_stick_x);
+        telemetry.addData("powerLY: ", gamepad1.left_stick_y);
+
+
+        telemetry.addData("FR START: ", FR.getCurrentPosition());
+        telemetry.addData("FL START: ", FL.getCurrentPosition());
+        telemetry.addData("BR START: ", BR.getCurrentPosition());
+        telemetry.addData("BL START: ", BL.getCurrentPosition());
+
+        telemetry.addData("Arm0: ", Arm0.getPower());
+        telemetry.addData("Arm0: ", Arm1.getPower());
+        telemetry.addData("Launcher: ", Launcher.getPosition());
+        telemetry.update();
+    }
+}
