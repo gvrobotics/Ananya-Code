cb888bcece10c2199f6dde8e61dc71b9de9f23c1
ananya4198 <115587326+ananya4198@users.noreply.github.com>
2024-01-27 13:12:44 -0500

    Add files via upload

--- Tele_AN.java
+++ Tele_AN.java
@@ -11,7 +11,7 @@ public class Tele_AN extends OpMode
 {
     private double powerLX, powerLY, powerRX, powerRY;
     private DcMotor FL, BL, FR, BR, Arm0, Arm1;
-    private Servo Launcher;
+    private Servo Launcher, Wrist, Claw;
 
     @Override
     public void init()
@@ -23,15 +23,16 @@ public class Tele_AN extends OpMode
         Arm0 = hardwareMap.get(DcMotor.class, "Arm0");
         Arm1 = hardwareMap.get(DcMotor.class, "Arm1");
         Launcher = hardwareMap.get(Servo.class, "Launcher");
+        Wrist = hardwareMap.get(Servo.class, "Wrist");
+        Claw = hardwareMap.get(Servo.class, "Claw");
 
 
-        FR.setDirection(DcMotorSimple.Direction.REVERSE);
-        FL.setDirection(DcMotorSimple.Direction.FORWARD);
-        BR.setDirection(DcMotorSimple.Direction.REVERSE);
-        BL.setDirection(DcMotorSimple.Direction.FORWARD);
+        BR.setDirection(DcMotorSimple.Direction.FORWARD);
+        BL.setDirection(DcMotorSimple.Direction.REVERSE);
+        FR.setDirection(DcMotorSimple.Direction.FORWARD);
+        FL.setDirection(DcMotorSimple.Direction.REVERSE);
         Arm0.setDirection(DcMotorSimple.Direction.REVERSE);
         Arm1.setDirection(DcMotorSimple.Direction.FORWARD);
-
 
         BR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -46,7 +47,8 @@ public class Tele_AN extends OpMode
         Arm0.setPower(0);
         Arm1.setPower(0);
         Launcher.setPosition(0);
-
+        Wrist.setPosition(0);
+        Claw.setPosition(0);
     }
 
     @Override
@@ -77,6 +79,7 @@ public class Tele_AN extends OpMode
             BR.setPower(0);
         }
 
+        //STRAFING
         if(gamepad1.right_bumper){
             FL.setPower(-0.85);
             BL.setPower(0.85);
@@ -92,55 +95,82 @@ public class Tele_AN extends OpMode
 
         //intake/outake
 
+        //ARM
         //going up
-        if (gamepad2.dpad_up)
-        {
-            Arm0.setPower(0.5);
-            Arm1.setPower(0.5);
-        }
-        //stopping when going up
-        if(gamepad2.dpad_right) {
-            Arm0.setPower(-0.15);
-            Arm1.setPower(-0.15);
+        if (gamepad2.dpad_up) {
+            Arm0.setPower(0.85);
+            Arm1.setPower(0.85);
         }
         //going down
         if(gamepad2.dpad_down){
-            Arm0.setPower(-0.2);
-            Arm1.setPower(-0.2);
+            Arm0.setPower(-0.3);
+            Arm1.setPower(-0.3);
         }
+        //stopping when going up
+        if(gamepad2.dpad_left) {
+            Arm0.setPower(-0.25);
+            Arm1.setPower(-0.25);
+        }
+        //RIGGING
+        //max power down
+        if(gamepad2.dpad_right) {
+            Arm0.setPower(-0.8);
+            Arm1.setPower(-0.8);
+        }
+        /*
         //stop when going down
         if(gamepad2.dpad_left){
-            Arm0.setPower(0.07);
-            Arm1.setPower(0.07);
+            Arm0.setPower(0.09);
+            Arm1.setPower(0.09);
         }
+        //to make it sit in the air
+        if(gamepad2.right_bumper){
+            Arm0.setPower(0.45);
+            Arm1.setPower(0.45);
+        }*/
         //stop TOTALLY / reset
         if(gamepad2.x){
             Arm0.setPower(0);
             Arm1.setPower(0);
         }
-        //to make it sit in the air
+
+        //WRIST
+        if(gamepad2.left_bumper){
+            Wrist.setPosition(0.8);
+        }
         if(gamepad2.right_bumper){
-            Arm0.setPower(0.35);
-            Arm1.setPower(0.35);
+            Wrist.setPosition(0);
         }
 
-
-        //to shoot launcher
-        if(gamepad2.a){
+        //LAUNCHER
+        if(gamepad1.a){
             Launcher.setPosition(0.4);
         }
-
         //reset launcher
-        if(gamepad2.y){
+        if(gamepad1.y){
             Launcher.setPosition(0);
         }
+
+        //CLAW
+        //open claw wide
+        if(gamepad2.y){
+            Claw.setPosition(0.25);
+        }
+        //open claw slightly
+        if(gamepad2.b){
+            Claw.setPosition(0.03);
+        }
+        //close claw
+        if(gamepad2.a){
+            Claw.setPosition(0);
+        }
+
 
         // telemetry
         telemetry.addData("powerRX: ", gamepad1.right_stick_x);
         telemetry.addData("powerRY: ", gamepad1.right_stick_y);
         telemetry.addData("powerLX: ", gamepad1.left_stick_x);
         telemetry.addData("powerLY: ", gamepad1.left_stick_y);
-
 
         telemetry.addData("FR START: ", FR.getCurrentPosition());
         telemetry.addData("FL START: ", FL.getCurrentPosition());
@@ -149,7 +179,10 @@ public class Tele_AN extends OpMode
 
         telemetry.addData("Arm0: ", Arm0.getPower());
         telemetry.addData("Arm0: ", Arm1.getPower());
+        telemetry.addData("Wrist: ", Wrist.getPosition());
         telemetry.addData("Launcher: ", Launcher.getPosition());
+        telemetry.addData("Claw: ", Claw.getPosition());
+
         telemetry.update();
     }
 }
