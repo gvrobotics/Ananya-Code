052ab93a0e2bf7cc1436f872aeebdd61133e615c
ananya4198 <115587326+ananya4198@users.noreply.github.com>
2025-11-04 11:24:23 -0500

    Refactor Tele5440 for improved control and structure

--- Decode/Tele5440.java
+++ Decode/Tele5440.java
@@ -1,4 +1,5 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.Tele;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -6,19 +7,16 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp
-public class Tele5440 extends OpMode
-{
+@TeleOp (name = "TeleOp 5440", group = "Tele")
+public class Tele5440 extends OpMode {
     public DcMotor BR, BL, FR, FL, spin1, spin2;
     private double powerRY, powerRX, powerLX, powerLY, robotAngle, PowerMultiplier, lf, rb, rf, lb;
     private Servo flip;
-    boolean motorOn, bumper;
     private ElapsedTime timer = new ElapsedTime();
-    private Boolean t = false;
+    private Boolean motorOn = false, t = false, previousGamepad = false, currentGamepad = false;
 
     @Override
-    public void init()
-    {
+    public void init() {
         BR = hardwareMap.get(DcMotor.class, "BR");
         BL = hardwareMap.get(DcMotor.class, "BL");
         FR = hardwareMap.get(DcMotor.class, "FR");
@@ -27,14 +25,13 @@ public class Tele5440 extends OpMode
         spin2 = hardwareMap.get(DcMotor.class, "s2");
         flip = hardwareMap.get(Servo.class, "f");
 
-
         BR.setDirection(DcMotorSimple.Direction.REVERSE);
         BL.setDirection(DcMotorSimple.Direction.FORWARD);
         FR.setDirection(DcMotorSimple.Direction.REVERSE);
         FL.setDirection(DcMotorSimple.Direction.FORWARD);
         spin1.setDirection(DcMotorSimple.Direction.FORWARD);
         spin2.setDirection(DcMotorSimple.Direction.REVERSE);
-        flip.setDirection(Servo.Direction.REVERSE);
+        flip.setDirection(Servo.Direction.FORWARD);
 
         BR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -49,24 +46,22 @@ public class Tele5440 extends OpMode
         FL.setPower(0);
         spin1.setPower(0);
         spin2.setPower(0);
-        flip.setPosition(0.85);
+        flip.setPosition(0);
     }
 
     @Override
-    public void loop()
-    {
-        powerLX = gamepad1.left_stick_x/1.4;
-        powerLY = gamepad1.left_stick_y/1.4;
-        powerRX = gamepad1.right_stick_x/1.4;
-        powerRY = gamepad1.right_stick_y/1.4;
+    public void loop() {
+        powerLX = gamepad1.left_stick_x;
+        powerLY = gamepad1.left_stick_y;
+        powerRX = gamepad1.right_stick_x;
 
         robotAngle = Math.atan2(powerLX, powerLY);
         PowerMultiplier = Math.sqrt((Math.pow(powerLX, 2) + Math.pow(powerLY, 2)));
 
-        lf = (PowerMultiplier*(Math.sin(robotAngle+(Math.PI/4)))) - powerRX;
-        rb = (PowerMultiplier*(Math.sin(robotAngle+(Math.PI/4)))) + powerRX;
-        lb = (PowerMultiplier*-1*(Math.sin(robotAngle-(Math.PI/4)))) - powerRX;
-        rf = (PowerMultiplier*-1*(Math.sin(robotAngle-(Math.PI/4)))) + powerRX;
+        lf = (PowerMultiplier * (Math.sin(robotAngle + (Math.PI / 4)))) - powerRX;
+        rb = (PowerMultiplier * (Math.sin(robotAngle + (Math.PI / 4)))) + powerRX;
+        lb = (PowerMultiplier * -1 * (Math.sin(robotAngle - (Math.PI / 4)))) - powerRX;
+        rf = (PowerMultiplier * -1 * (Math.sin(robotAngle - (Math.PI / 4)))) + powerRX;
 
         BR.setPower(rb);
         BL.setPower(lb);
@@ -74,49 +69,35 @@ public class Tele5440 extends OpMode
         FL.setPower(lf);
 
         // Intake
-        if (gamepad1.x) {
-            //Set to 0.7 when X is pressed
-            flip.setPosition(0.7);
-            t = false;
-        }
-        else if (gamepad1.y && !t) {
+        if (gamepad1.right_bumper && !t) {
             // When Y is pressed, flip down
-            flip.setPosition(0.7);
+            flip.setPosition(0.2);
             timer.reset();
             t = true;
-        }
-        else if (t && !gamepad1.y && timer.seconds() > 0.2) {
-            // After Y is released for 0.2s, flip back up
-            flip.setPosition(0.85);
+        } else if (t && !gamepad1.right_bumper && timer.seconds() > 0.3) {
+            // After Y is released for 0.1s, flip back up
+            flip.setPosition(0);
             t = false;
         }
-/*
-        if (gamepad1.dpad_up) {
-            spin1.setPower(0.5);
-            spin2.setPower(0.5);
-        } else if (gamepad1.dpad_down) {
-            spin1.setPower(0.4);
-            spin2.setPower(0.4);
-        } else if (gamepad1.a) {
-            spin1.setPower(0);
-            spin2.setPower(0);
-        }
-*/
-        //when bumpers pressed
-        if (gamepad1.left_bumper) {
-            motorOn = !motorOn;  // toggle the state (true -> false, false -> true)
-        }
 
-        // Run or stop the motor depending on the current state
-        if (motorOn) { //on
-            spin1.setPower(0.4);
-            spin2.setPower(0.4);
-        }
-        if (!motorOn) { //off
-            spin1.setPower(0.0);
-            spin2.setPower(0.0);
-        }
+        
+        //TOGGLE
+        // Stores the previous state of the button
+        previousGamepad = currentGamepad;
+        // Read the current state of bumper
+        currentGamepad = gamepad1.left_bumper;
 
+        // Checks if the button was just pressed
+        if (currentGamepad && !previousGamepad) {
+            motorOn = !motorOn;
+            if (motorOn) {
+                spin1.setPower(0.45);
+                spin2.setPower(0.45);
+            } else {
+                spin1.setPower(0);
+                spin2.setPower(0);
+            }
+        }
 
         telemetry.addData("Robot angle:", robotAngle);
         telemetry.addData("powerRX: ", gamepad1.right_stick_x);
