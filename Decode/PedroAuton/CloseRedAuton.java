87235bd4485d8a56829c2b09e16a9dd5b48a75a8
ananya4198 <115587326+ananya4198@users.noreply.github.com>
2026-01-16 20:54:24 -0500

    Update CloseRedAuton.java

--- Decode/PedroAuton/CloseRedAuton.java
+++ Decode/PedroAuton/CloseRedAuton.java
@@ -12,7 +12,12 @@ import com.pedropathing.util.Timer;
 public class CloseRedAuton extends OpMode {
 
     private Follower follower;
-    private Timer pathTimer, opModeTimer;
+    private Timer pathTimer, opModeTimer, intakeTimer;
+
+    private boolean firstRowPathStarted = false;
+    private boolean secondRowPathStarted = false;
+
+
 
     // ---------- FLYWHEEL SETUP ----------
     private Flywheel shooter = new Flywheel();
@@ -24,20 +29,32 @@ public class CloseRedAuton extends OpMode {
         // SHOOT -> ATTEMPT TO SCORE ARTIFACT
         DRIVE_STARTPOS_SHOOT_POS,
         SHOOT_PRELOAD,
+        //------FIRST ROW------
         DRIVE_SHOOTPOS_FIRSTROW_START,
         DRIVE_FIRSTROW_START_TO_END,
-        DRIVE_SHOOT_ENDPOS
+        DRIVE_FIRST_ENDPOS_SHOOTPOS,
+        SHOOT_FIRSTROW,
+        //-----SECOND ROW-------
+        DRIVE_SHOOT_TO_SECONDROW_START,
+        DRIVE_SECONDROW_START_TO_END,
+        DRIVE_SECOND_ENDPOS_SHOOTPOS,
+        SHOOT_SECONDROW,
+        DRIVE_SHOOT_ENDPOS;
     }
 
 
     PathState pathState;
 
-    private final Pose startPose = new Pose(110.05892547660312, 135.51473136915078, Math.toRadians(90));
-    private final Pose shootPose = new Pose(83.35528596187177, 90.84228769497398, Math.toRadians(45));
-    private final Pose firstRowStart = new Pose(101.94627383015599, 83.6065857885615, Math.toRadians(0));
-    private final Pose firstRowEnd = new Pose(137.98786828422877, 83.6065857885615, Math.toRadians(0));
-    private final Pose endPose = new Pose(71.74870017331021, 103.22876949740034, Math.toRadians(180)); //add ending pose here
-    private PathChain driveStartPosShootPos, driveShootPosFirstRowStartPose, driveFirstRowStartToEndPose, driveShootPosEndPose;
+    private final Pose startPose = new Pose(117.54592720970538, 129.2755632582322, Math.toRadians(45));
+    private final Pose shootPose = new Pose(92.63570190641249, 99.3275563258232, Math.toRadians(45));
+    private final Pose secondRowShootPose = new Pose(83.9116117850953, 135.63778162911612, Math.toRadians(0));
+    private final Pose firstRowStart = new Pose(100.69844020797227, 83.6065857885615, Math.toRadians(0));
+    //TODO Change Y Values
+    private final Pose firstRowEnd = new Pose(129.00346620450608, 83.6065857885615, Math.toRadians(0));
+    private final Pose secondRowStart = new Pose(100.69844020797227, 59.82668977469672, Math.toRadians(0));
+    private final Pose secondRowEnd = new Pose(129.00346620450608, 59.82668977469672, Math.toRadians(0));
+    private final Pose endPose = new Pose(101.2512998266898, 71.06759098786829, Math.toRadians(0));
+    private PathChain driveStartPosShootPos, driveShootPosFirstRowStartPose, driveFirstRowStartToEndPose, driveFirstRowtoShootPose, driveShootPosSecondRowStartPose, driveSecondRowStartToEndPose, driveSecondRowtoShootPose, driveShootPosEndPose;
 
     public void buildPaths() {
         // Put in coordinates for starting pose -> ending pose
@@ -45,6 +62,7 @@ public class CloseRedAuton extends OpMode {
                 .addPath(new BezierLine(startPose, shootPose))
                 .setLinearHeadingInterpolation(startPose.getHeading(),shootPose.getHeading())
                 .build();
+        //---------------------FIRST ROW START----------------------
         driveShootPosFirstRowStartPose = follower.pathBuilder()
                 .addPath(new BezierLine(shootPose, firstRowStart))
                 .setLinearHeadingInterpolation(shootPose.getHeading(),firstRowStart.getHeading())
@@ -53,43 +71,42 @@ public class CloseRedAuton extends OpMode {
                 .addPath(new BezierLine(firstRowStart, firstRowEnd))
                 .setLinearHeadingInterpolation(firstRowStart.getHeading(),firstRowEnd.getHeading())
                 .build();
+        driveFirstRowtoShootPose = follower.pathBuilder()
+                .addPath(new BezierLine(firstRowEnd, shootPose))
+                .setLinearHeadingInterpolation(firstRowEnd.getHeading(), shootPose.getHeading())
+                .build();
+        //---------------------SECOND ROW START------------------------
+        driveShootPosSecondRowStartPose = follower.pathBuilder()
+                .addPath(new BezierLine(shootPose, secondRowStart))
+                .setLinearHeadingInterpolation(shootPose.getHeading(),firstRowStart.getHeading())
+                .build();
+        driveSecondRowStartToEndPose = follower.pathBuilder()
+                .addPath(new BezierLine(secondRowStart, secondRowEnd))
+                .setLinearHeadingInterpolation(secondRowStart.getHeading(), secondRowEnd.getHeading())
+                .build();
+        driveSecondRowtoShootPose = follower.pathBuilder()
+                .addPath(new BezierLine(secondRowEnd, secondRowShootPose))
+                .setLinearHeadingInterpolation(secondRowEnd.getHeading(), secondRowShootPose.getHeading())
+                .build();
+
         driveShootPosEndPose = follower.pathBuilder()
                 .addPath(new BezierLine(shootPose, endPose))
                 .setLinearHeadingInterpolation(firstRowEnd.getHeading(), endPose.getHeading())
                 .build();
+
     }
 
     public void statePathUpdate(){
         switch(pathState) {
             case DRIVE_STARTPOS_SHOOT_POS:
                 follower.followPath(driveStartPosShootPos, true);
+                shooter.fly1.setVelocity(1300);
+                shooter.fly2.setVelocity(1300);
                 setPathState(PathState.SHOOT_PRELOAD); //reset timer & make new state
                 break;
 
-            case DRIVE_SHOOTPOS_FIRSTROW_START:
-                follower.followPath(driveShootPosFirstRowStartPose, true);
-                shooter.intake1.setVelocity(1500);
-                shooter.intake2.setVelocity(1500);
-                setPathState(PathState.DRIVE_FIRSTROW_START_TO_END);
-                break;
-
-            case DRIVE_FIRSTROW_START_TO_END:
-                follower.followPath(driveFirstRowStartToEndPose, true);
-
-                // KEEP intake on while driving
-                shooter.intake1.setVelocity(1500);
-                shooter.intake2.setVelocity(1500);
-
-                // ONLY transition once path is finished
-                if (!follower.isBusy()) {
-                    shooter.intake1.setVelocity(0);
-                    shooter.intake2.setVelocity(0);
-                    setPathState(PathState.DRIVE_SHOOT_ENDPOS);
-                }
-                break;
-
             case SHOOT_PRELOAD:
-                // Check if follower done it's path?
+                // Check if follower done it's path
                 if (!follower.isBusy()) {
                     // requested shots yet
                     if (!shotsTriggered) {
@@ -100,6 +117,97 @@ public class CloseRedAuton extends OpMode {
                         // shots are done free to transition
                         follower.followPath(driveShootPosFirstRowStartPose, true);
                         setPathState(PathState.DRIVE_SHOOTPOS_FIRSTROW_START);
+                    }
+                }
+                break;
+
+//-------------------------------FIRST ROW--------------------------------------
+            case DRIVE_SHOOTPOS_FIRSTROW_START:
+                follower.followPath(driveShootPosFirstRowStartPose, true);
+                shooter.intake1.setPower(0.7);
+                shooter.intake2.setPower(0.7);
+                setPathState(PathState.DRIVE_FIRSTROW_START_TO_END);
+                intakeTimer.resetTimer();
+                break;
+
+            case DRIVE_FIRSTROW_START_TO_END:
+                if (!firstRowPathStarted) {
+                    follower.followPath(driveFirstRowStartToEndPose, true);
+                    firstRowPathStarted = true;
+                }
+
+                if (!follower.isBusy()) {
+                    shooter.intake1.setPower(0);
+                    shooter.intake2.setPower(0);
+                    firstRowPathStarted = false;
+                    setPathState(PathState.DRIVE_FIRST_ENDPOS_SHOOTPOS);
+                }
+                break;
+
+            case DRIVE_FIRST_ENDPOS_SHOOTPOS:
+                follower.followPath(driveFirstRowtoShootPose, true);
+                shooter.fly1.setVelocity(1300);
+                shooter.fly2.setVelocity(1300);
+                setPathState(PathState.SHOOT_FIRSTROW);
+                break;
+
+            case SHOOT_FIRSTROW:
+                // Check if follower done it's path
+                if (!follower.isBusy()) {
+                    // requested shots yet
+                    if (!shotsTriggered) {
+                        shooter.fireShots(3);
+                        shotsTriggered = true;
+                    }
+                    else if (shotsTriggered && !shooter.isBusy()) {
+                        // shots are done free to transition
+                        follower.followPath(driveShootPosSecondRowStartPose, true);
+                        setPathState(PathState.DRIVE_SHOOT_TO_SECONDROW_START);
+                    }
+                }
+                break;
+//-------------------------------SECOND ROW--------------------------------------
+            case DRIVE_SHOOT_TO_SECONDROW_START:
+                follower.followPath(driveShootPosSecondRowStartPose, true);
+                shooter.intake1.setPower(0.7);
+                shooter.intake2.setPower(0.7);
+                setPathState(PathState.DRIVE_SECONDROW_START_TO_END);
+                intakeTimer.resetTimer();
+                break;
+
+            case DRIVE_SECONDROW_START_TO_END:
+                if (!secondRowPathStarted) {
+                    follower.followPath(driveSecondRowStartToEndPose, true);
+                    secondRowPathStarted = true;
+                }
+
+                if (!follower.isBusy()) {
+                    shooter.intake1.setPower(0);
+                    shooter.intake2.setPower(0);
+                    secondRowPathStarted = false;
+                    setPathState(PathState.DRIVE_SECOND_ENDPOS_SHOOTPOS);
+                }
+                break;
+
+            case DRIVE_SECOND_ENDPOS_SHOOTPOS:
+                follower.followPath(driveSecondRowtoShootPose, true);
+                shooter.fly1.setVelocity(1300);
+                shooter.fly2.setVelocity(1300);
+                setPathState(PathState.SHOOT_SECONDROW);
+                break;
+
+            case SHOOT_SECONDROW:
+                // Check if follower done it's path
+                if (!follower.isBusy()) {
+                    // requested shots yet
+                    if (!shotsTriggered) {
+                        shooter.fireShots(3);
+                        shotsTriggered = true;
+                    }
+                    else if (shotsTriggered && !shooter.isBusy()) {
+                        // shots are done free to transition
+                        follower.followPath(driveShootPosEndPose, true);
+                        setPathState(PathState.DRIVE_SHOOT_ENDPOS);
                     }
                 }
                 break;
@@ -120,7 +228,8 @@ public class CloseRedAuton extends OpMode {
     public void setPathState(PathState newState) {
         pathState = newState;
         pathTimer.resetTimer();
-
+        firstRowPathStarted = false;
+        secondRowPathStarted = false;
         shotsTriggered = false;
     }
 
@@ -129,6 +238,7 @@ public class CloseRedAuton extends OpMode {
         pathState = pathState.DRIVE_STARTPOS_SHOOT_POS;
         pathTimer = new Timer();
         opModeTimer = new Timer();
+        intakeTimer = new Timer();
         follower = Constants.createFollower(hardwareMap);
         // TODO add in any other init mechanisms
         shooter.init(hardwareMap);
